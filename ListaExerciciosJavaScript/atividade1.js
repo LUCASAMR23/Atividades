4f7be0665cd425f90b7a1db22a6a42b3a45cadc1
Lucas Amorim <lucasrodriguessptv@gmail.com>
2026-07-13 20:11:18 -0300

    Atividade com foco no desenvolvimento da logica, nesse exercicio solicitei a entrada de um numero ao usuario, assim que ele digita os dois numeros aparece a soma deles

---
+++ ListaExerciciosJavaScript/atividade1.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt("Digite um numero: "));
+let numero2 = Number(prompt("Digite o segundo numero"));
+
+
+let Soma = numero1 + numero2;
+
+console.log("O resultado final é:",Soma);
