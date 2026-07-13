75597a7e41f48e65dafc976705509a785748be51
Lucas Amorim <lucasrodriguessptv@gmail.com>
2026-07-13 20:30:18 -0300

    Nessa segunda atividade foi desenvolvido um exercicio de logica que faz com que o usuario digite 4 notas e o sistema tira a média final

---
+++ ListaExerciciosJavaScript/atividade2.js
@@ -0,0 +1,8 @@
+let nota1 = Number(prompt("Digite a sua primeira nota: "));
+let nota2 = Number(prompt("Digite a sua segunda nota: "));
+let nota3 = Number(prompt("Digite a sua terceira nota: "));
+let nota4 = Number(prompt("Digite a sua quarta nota: "));
+
+let media = (nota1 + nota2 + nota3 + nota4)/ 4;
+
+console.log("A sua média foi de ", media);
