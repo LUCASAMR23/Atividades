7b0b1634a7ab46fb4397a1bbe825cd576a279f66
Lucas Amorim <lucasrodriguessptv@gmail.com>
2026-07-13 20:38:50 -0300

    Pequeno sistema que identifica se o numero é positivo, negativo ou 0.
    (Achei um exercicio facil)---- Nível Facil

---
+++ ListaExerciciosJavaScript/atividade4.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um numero: "))
+
+if(numero > 0){
+    console.log("Esse numero é positivo");
+}else if(numero < 0 ){
+    console.log("Esse numero é negativo");
+}else{
+    console.log("Esse numero é o 0");
+}
